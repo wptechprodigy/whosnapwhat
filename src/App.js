1969ca90dad57b101e68ea6db67be82e0fc5406b
wptechprodigy <waheed.freelancer16@gmail.com>
2020-04-13 12:12:08 +0100

    refactor app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ImageCard from './components/ImageCard';
 
 function App() {
+	const [images, setImages] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [term, setTerm] = useState('');
+
+	useEffect(() => {
+		const fetchImage = async () => {
+			const result = await axios(
+				`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`,
+			);
+
+			setImages(result.data.hits);
+			setIsLoading(false);
+		};
+
+		fetchImage();
+	}, [term]);
+
 	return (
-		<div class='max-w-sm rounded overflow-hidden shadow-lg'>
-			<img src='https://source.unsplash.com/random' alt='' className='w-full' />
-			<div className='px-6 py-4'>
-				<div className='font-bold text-purple-500 mb-2'>The Coldest Sunset</div>
-				<p className='text-gray-700 text-base'>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-					quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-					nihil.
-				</p>
-			</div>
-			<div class='px-6 py-4'>
-				<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-					#photography
-				</span>
-				<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-					#travel
-				</span>
-				<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>
-					#winter
-				</span>
-			</div>
+		<div className='container mx-auto'>
+			{isLoading ? (
+				<h1 className='text-center text-gray-700 text-6xl'>Loading...</h1>
+			) : (
+				<div className='grid grid-cols-3 gap-4'>
+					{images.map((image) => (
+						<ImageCard image={image} key={image.id} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
