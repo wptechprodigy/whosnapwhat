0c05234bf125a21d76ab4bae0fb9f777cfe8e8b2
wptechprodigy <waheed.freelancer16@gmail.com>
2020-04-13 12:11:52 +0100

    create tag component

---
+++ src/components/Tags.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Tags({ tag }) {
+	return (
+		<span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+			#{tag}
+		</span>
+	);
+}
+
+export default Tags;
