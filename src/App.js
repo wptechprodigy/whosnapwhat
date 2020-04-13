4edcd0b4eb5370a454c7c466346f1f1c3904f107
wptechprodigy <waheed.freelancer16@gmail.com>
2020-04-13 07:45:19 +0100

    set up app with Tailwind css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div class='max-w-sm rounded overflow-hidden shadow-lg'>
+			<img src='https://source.unsplash.com/random' alt='' className='w-full' />
+			<div className='px-6 py-4'>
+				<div className='font-bold text-purple-500 mb-2'>The Coldest Sunset</div>
+				<p className='text-gray-700 text-base'>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+					quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+					nihil.
+				</p>
+			</div>
+			<div class='px-6 py-4'>
+				<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+					#photography
+				</span>
+				<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+					#travel
+				</span>
+				<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>
+					#winter
+				</span>
+			</div>
+		</div>
+	);
 }
 
 export default App;
