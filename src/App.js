181df43b94312cc70c7a6a2d1ab8db7adb354102
wptechprodigy <waheed.freelancer16@gmail.com>
2020-04-14 09:28:17 +0100

    add error handling functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ImageCard from './components/ImageCard';
+import DisplayError from './components/DisplayError';
 
 function App() {
 	const [images, setImages] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
 	const [term, setTerm] = useState('');
+	const [isError, setIsError] = useState(false);
 
 	useEffect(() => {
 		const fetchImage = async () => {
-			const result = await axios(
-				`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`,
-			);
+			setIsError(false);
+			setIsLoading(true);
 
-			setImages(result.data.hits);
-			setIsLoading(false);
+			try {
+				const result = await axios(
+					`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`,
+				);
+
+				setImages(result.data.hits);
+			} catch (error) {
+				setIsError(true);
+			}
 		};
+
+		setIsLoading(false);
 
 		fetchImage();
 	}, [term]);
 
 	return (
-		<div className='container mx-auto'>
+		<div className='container mx-auto my-10'>
+			{isError && <DisplayError message='Something went wrong.' />}
+
 			{isLoading ? (
 				<h1 className='text-center text-gray-700 text-6xl'>Loading...</h1>
 			) : (
-				<div className='grid grid-cols-3 gap-4'>
+				<div className='flex flex-col justify-center items-center md:justify-center md:grid md:grid-cols-2 md:gap-4 lg:grid lg:grid-cols-3 lg:gap-4 place-items-center'>
 					{images.map((image) => (
 						<ImageCard image={image} key={image.id} />
 					))}
