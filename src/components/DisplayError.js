be909b62054c3b1bbb8d342c38d4bf9fbf16e334
wptechprodigy <waheed.freelancer16@gmail.com>
2020-04-14 13:03:49 +0100

    add margin top and bottom on error message

--- src/components/DisplayError.js
+++ src/components/DisplayError.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function DisplayError({ message }) {
-	return <h1 className='text-center text-gray-700 text-6xl'>{message}</h1>;
+	return <h1 className='text-center text-gray-700 text-6xl my-10'>{message}</h1>;
 }
 
 export default DisplayError;
