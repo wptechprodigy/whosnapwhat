83941e1dd778e50bab079cd5800d36b94517140b
wptechprodigy <waheed.freelancer16@gmail.com>
2020-04-15 02:11:29 +0100

    refactor app to usePixabayApi hook

--- src/App.js
+++ src/App.js
@@ -1,63 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import ImageCard from './components/ImageCard';
 import DisplayError from './components/DisplayError';
 import ImageSearch from './components/ImageSearch';
+import Navigation from './components/Navigation';
+import usePixabayApi from './hooks/usePixabayApi';
 
 function App() {
-	const [images, setImages] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [term, setTerm] = useState('computers');
-	const [isError, setIsError] = useState(false);
-
-	useEffect(() => {
-		const fetchImage = async () => {
-			setIsError(false);
-			setIsLoading(true);
-
-			try {
-				const result = await axios(
-					`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`,
-				);
-
-				setImages(result.data.hits);
-				setIsLoading(false);
-			} catch (error) {
-				setIsError(true);
-			}
-		};
-
-		fetchImage();
-	}, [term]);
+	const [{ images, isLoading, isError, term }, setTerm] = usePixabayApi();
 
 	return (
-		<div className='container mx-auto my-10'>
-			<ImageSearch searchTerm={(text) => setTerm(text)} />
+		<>
+			<Navigation />
+			<div className='container mx-auto my-10'>
+				<ImageSearch searchTerm={(text) => setTerm(text)} />
 
-			{isError && <DisplayError message='Something went wrong.' />}
+				{isError && <DisplayError message='Something went wrong.' />}
 
-			{!isLoading && images.length === 0 && (
-				<h1 className='text-center text-gray-700 text-6xl my-10'>
-					No Images Found.
-				</h1>
-			)}
-
-			{isLoading ? (
-				isError ? (
-					''
-				) : (
+				{!isLoading && images.length === 0 && (
 					<h1 className='text-center text-gray-700 text-6xl my-10'>
-						Loading...
+						No Images Found.
 					</h1>
-				)
-			) : (
-				<div className='flex flex-col justify-center items-center md:justify-center md:grid md:grid-cols-2 md:gap-4 lg:grid lg:grid-cols-3 lg:gap-4 place-items-center'>
-					{images.map((image) => (
-						<ImageCard image={image} key={image.id} />
-					))}
-				</div>
-			)}
-		</div>
+				)}
+
+				{isLoading ? (
+					isError ? (
+						''
+					) : (
+						<h1 className='text-center text-gray-700 text-6xl my-10'>
+							Loading...
+						</h1>
+					)
+				) : (
+					<>
+						<h3 className='text-center text-gray-700 text-xl mb-5'>
+							Showing {images.length} results for {term}
+						</h3>
+
+						<div className='flex flex-col justify-center items-center md:justify-center md:grid md:grid-cols-2 md:gap-4 lg:grid lg:grid-cols-3 lg:gap-4 place-items-center'>
+							{images.map((image) => (
+								<ImageCard image={image} key={image.id} />
+							))}
+						</div>
+					</>
+				)}
+			</div>
+		</>
 	);
 }
 
